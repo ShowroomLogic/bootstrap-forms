7ca127d097b99c0239abf3aa0f311955cd3559cc
Chris Brantley <chris@showroomlogic.com>
2017-02-01 08:44:04 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Bootstrap Forms
+===============
+
+A collection of Django templates for rendering forms using Bootstrap 3.x markup.
+
+Installation
+------------
+
+Add `bootstrap_forms` to your INSTALLED_APPS setting.
---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import find_packages, setup
+from django_helpers import __version__, __author__, __author_email__
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='bootstrap-forms',
+    version=__version__,
+    description='Collection of templates for rendering django forms with Bootstrap markup.',
+    url='https://github.com/ShowroomLogic/bootstrap-forms',
+    author=__author__,
+    author_email=__author_email__,
+    packages=find_packages(),
+    zip_safe=False
+)
